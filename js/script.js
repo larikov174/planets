07ea267c70ddfaf5fe63b6487d0b07fca6615ec9
Andrey Larikov <larikov174@gmail.com>
2021-07-07 00:35:56 +0500

    feature/js: script logic added

--- js/script.js
+++ js/script.js
@@ -1,22 +1,54 @@
 'use strict';
 document.addEventListener('DOMContentLoaded', () => {
-  const buttons = document.querySelectorAll('.main__button'),
+  const
+    buttons = document.querySelectorAll('.main__button'),
+    overviewBtn = document.querySelector('[data-name="overview"]'),
     planets = document.querySelectorAll('.header__link'),
+    mTitle = document.querySelector('.main__title'),
+    mText = document.querySelector('.main__text'),
+    mLink = document.querySelector('.main__link'),
+    mImage = document.querySelector('.main__image'),
     cards = document.querySelector('.cards');
   let index = 0;
+
 
   // -----------header planets block--------------
   planets.forEach((item) => {
     item.addEventListener('click', () => {
+      //---------setting overview as default active button----
+      for (let button of buttons) {
+        if (button.classList.contains('main__button_active')) {
+          button.classList.remove('main__button_active');
+        }
+      }
+      overviewBtn.classList.add('main__button_active');
+
       index = item.dataset.id;
-      console.log(++index);
+      fetch('data.json')
+        .then((Response) => Response.json())
+        .then((json) => {
+          // -----------cards block--------------
+          cards.childNodes[1].childNodes[3].textContent = json[index].rotation;
+          cards.childNodes[3].childNodes[3].textContent = json[index].revolution;
+          cards.childNodes[5].childNodes[3].textContent = json[index].radius;
+          cards.childNodes[7].childNodes[3].textContent = json[index].temperature;
+          //------------main title block --------
+          mTitle.textContent = json[index].name;
+          //------------main text block --------
+          mText.textContent = json[index].overview.content;
+          //------------main link block --------
+          mLink.href = json[index].overview.source;
+          //------------main image block --------
+          mImage.alt = json[index].name;
+          mImage.src = json[index].images.planet;
+        });
     });
   });
 
   // -----------main nav buttons block--------------
-
   buttons.forEach((item) => {
     item.addEventListener('mousedown', () => {
+      //-------hover/active toggle-------
       for (let button of buttons) {
         if (button.classList.contains('main__button_active')) {
           button.classList.remove('main__button_active');
@@ -25,17 +57,20 @@ document.addEventListener('DOMContentLoaded', () => {
       }
       item.classList.toggle('main__button_hover');
       item.classList.toggle('main__button_active');
+      //-------------info change----------
+      let option = item.dataset.name;
+      let picture = item.dataset.image;
+      fetch('data.json')
+        .then((Response) => Response.json())
+        .then((json) => {
+          //------------main text block --------
+          mText.textContent = json[index][option].content;
+          //------------main link block --------
+          mLink.href = json[index][option].source;
+          //------------main image block --------
+          mImage.src = json[index].images[picture];
+        });
     });
   });
 
-  // -----------cards block--------------
-
-  fetch('data.json')
-    .then((Response) => Response.json())
-    .then((json) => {
-      cards.childNodes[1].childNodes[3].textContent = json[index].rotation;
-      cards.childNodes[3].childNodes[3].textContent = json[0].revolution;
-      cards.childNodes[5].childNodes[3].textContent = json[0].radius;
-      cards.childNodes[7].childNodes[3].textContent = json[0].temperature;
-    });
 });
