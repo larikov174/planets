b6378e3c6b79d2467dfd93b80d9ff33d10530c32
Andrey Larikov <larikov174@gmail.com>
2021-07-07 22:24:32 +0500

    develop: js logic dune, minor html fix, data.json update

--- js/script.js
+++ js/script.js
@@ -1,84 +1,99 @@
-'use strict';
-document.addEventListener('DOMContentLoaded', () => {
-  const
-    buttons = document.querySelectorAll('.main__button'),
+"use strict";
+document.addEventListener("DOMContentLoaded", () => {
+  const buttons = document.querySelectorAll(".main__button"),
     overviewBtn = document.querySelector('[data-name="overview"]'),
-    planets = document.querySelectorAll('.header__link'),
-    mTitle = document.querySelector('.main__title'),
-    mText = document.querySelector('.main__text'),
-    mLink = document.querySelector('.main__link'),
-    mImage = document.querySelector('.main__image'),
-    cards = document.querySelector('.cards');
+    planets = document.querySelectorAll(".header__link"),
+    mTitle = document.querySelector(".main__title"),
+    mText = document.querySelector(".main__text"),
+    mLink = document.querySelector(".main__link"),
+    mImage = document.querySelector(".main__image"),
+    mImagePointer = document.querySelector(".main__image-pointer"),
+    cards = document.querySelector(".cards");
 
-  let
-    index = 0;
+  let index = 0;
 
   function btnStatusChange() {
     for (let button of buttons) {
-      button.classList.remove('main__button_active');
-      button.classList.add('main__button_hover');
+      button.classList.remove("main__button_active");
+      button.classList.add("main__button_hover");
+      button.style.backgroundColor = ""; //inline styles remove
     }
   }
 
   function headerLinksStatusChange() {
     for (let planet of planets) {
-      planet.classList.remove('header__link_active');
+      planet.classList.remove("header__link_active");
     }
   }
 
-  // -----------header planets block--------------
+  // -----------header interaction--------------
   planets.forEach((planet) => {
-    planet.addEventListener('click', () => {
-      //---------setting overview as default active button----
+    planet.addEventListener("mouseup", () => {
+      //---------setting default active/hover state----
       btnStatusChange();
-      overviewBtn.classList.add('main__button_active');
-      overviewBtn.classList.toggle('main__button_hover');
+      overviewBtn.classList.add("main__button_active");
+      overviewBtn.classList.toggle("main__button_hover");
       headerLinksStatusChange();
-      planet.classList.add('header__link_active');
+      planet.classList.add("header__link_active");
+      mImagePointer.classList.remove("main__image-pointer_active");
 
       index = planet.dataset.id;
-      fetch('data.json')
+      fetch("data.json")
         .then((Response) => Response.json())
         .then((json) => {
+          //------header color-scheme switch-----
+          planet.style.borderTopColor = json[index].color;
+          //--fist button color-scheme switch---
+          overviewBtn.style.backgroundColor = json[index].color;
+          //------------main title block --------
+          mTitle.textContent = json[index].name;
+          //------------main text block ---------
+          mText.textContent = json[index].overview.content;
+          //------------main link block ---------
+          mLink.href = json[index].overview.source;
+          //------------main image block --------
+          mImage.alt = json[index].name;
+          mImage.src = json[index].images.planet;
+          mImage.style.width = json[index].images.size;
           // -----------cards block--------------
           cards.childNodes[1].childNodes[3].textContent = json[index].rotation;
           cards.childNodes[3].childNodes[3].textContent = json[index].revolution;
           cards.childNodes[5].childNodes[3].textContent = json[index].radius;
           cards.childNodes[7].childNodes[3].textContent = json[index].temperature;
-          //------------main title block --------
-          mTitle.textContent = json[index].name;
-          //------------main text block --------
-          mText.textContent = json[index].overview.content;
-          //------------main link block --------
-          mLink.href = json[index].overview.source;
-          //------------main image block --------
-          mImage.alt = json[index].name;
-          mImage.src = json[index].images.planet;
         });
     });
   });
 
   // -----------main nav buttons block--------------
   buttons.forEach((button) => {
-    button.addEventListener('mousedown', () => {
+    button.addEventListener("mousedown", () => {
       //-------hover/active toggle-------
       btnStatusChange();
-      button.classList.toggle('main__button_hover');
-      button.classList.toggle('main__button_active');
+      button.classList.toggle("main__button_hover");
+      button.classList.toggle("main__button_active");
       //-------------info change----------
       let option = button.dataset.name;
       let picture = button.dataset.image;
-      fetch('data.json')
+      fetch("data.json")
         .then((Response) => Response.json())
         .then((json) => {
+          //--main buttons color-scheme switch---
+          button.style.backgroundColor = json[index].color;
           //------------main text block --------
           mText.textContent = json[index][option].content;
           //------------main link block --------
           mLink.href = json[index][option].source;
           //------------main image block --------
           mImage.src = json[index].images[picture];
+          mImagePointer.src = json[index].images.geology;
+          mImagePointer.alt = json[index].name;
+          mImagePointer.classList.remove("main__image-pointer_active");
+          switch (button.dataset.name) {
+            case "geology":
+              mImagePointer.classList.add("main__image-pointer_active");
+              break;
+          }
         });
     });
   });
-
 });
